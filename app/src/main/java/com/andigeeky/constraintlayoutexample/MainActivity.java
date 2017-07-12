bfac286cb523c3b7ce4ac4b5bd52409562e01210
mamatagelanee07 <gelaneem@gmail.com>
2017-07-12 08:59:54 +0530

    start another activity using intent

--- app/src/main/java/com/andigeeky/constraintlayoutexample/MainActivity.java
+++ app/src/main/java/com/andigeeky/constraintlayoutexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.andigeeky.constraintlayoutexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String KEY_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Called when user tap on send button
+     *
      * @param view instance of Button
      */
     public void sendMessage(View view) {
-
+        String message = ((EditText) findViewById(R.id.edt_name)).getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra(KEY_MESSAGE, message);
+        startActivity(intent);
     }
 }
