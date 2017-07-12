4537b4408e50d58dc52ae9f4f19636f709f29dbd
mamatagelanee07 <gelaneem@gmail.com>
2017-07-12 09:09:21 +0530

    get message and display in different activity

--- app/src/main/java/com/andigeeky/constraintlayoutexample/DisplayMessageActivity.java
+++ app/src/main/java/com/andigeeky/constraintlayoutexample/DisplayMessageActivity.java
@@ -1,7 +1,9 @@
 package com.andigeeky.constraintlayoutexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getExtras().getString(MainActivity.KEY_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.lbl_txt_message);
+        textView.setText(message);
     }
 }
