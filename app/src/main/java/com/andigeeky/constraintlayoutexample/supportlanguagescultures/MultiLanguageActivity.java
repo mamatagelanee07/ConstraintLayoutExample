24914138fcd32b2e61cb64df980e4c3bf523d1b7
Gelanee, Mamata <Mamata.Gelanee@mastercard.com>
2017-07-21 11:32:04 +0530

    Added flag images

---
+++ app/src/main/java/com/andigeeky/constraintlayoutexample/supportlanguagescultures/MultiLanguageActivity.java
@@ -0,0 +1,15 @@
+package com.andigeeky.constraintlayoutexample.supportlanguagescultures;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.andigeeky.constraintlayoutexample.R;
+
+public class MultiLanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_language);
+    }
+}
