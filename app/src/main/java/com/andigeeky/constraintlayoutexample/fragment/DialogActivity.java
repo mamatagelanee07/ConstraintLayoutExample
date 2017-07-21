a44c8466aad74709584c52bac802ab80ed76e0f8
Gelanee, Mamata <Mamata.Gelanee@mastercard.com>
2017-07-21 12:05:18 +0530

    Added empty activity

---
+++ app/src/main/java/com/andigeeky/constraintlayoutexample/fragment/DialogActivity.java
@@ -0,0 +1,15 @@
+package com.andigeeky.constraintlayoutexample.fragment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.andigeeky.constraintlayoutexample.R;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+    }
+}
